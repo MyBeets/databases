79ef4d4e524e08b5f7c600ebae4decfd78032ad0
MyBeets <ikeller2023@kehillahstudent.org>
2022-09-01 14:32:27 -0700

    hi

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main{
+	int pass = 0;
+	int fail = 0;
+	public static void main(String[] args) {
+	    System.out.println("mingus");
+	    stack ms = new stack(1);
+	    System.out.println(ms.stack);
+	}
+	public static void testpop(){
+
+	}
+}
---
+++ stack.java
@@ -0,0 +1,13 @@
+public class stack{
+	public static void stack(){
+	}
+	public static void stack(int size){
+		public int[] stack = int[size];
+	}
+	public int pop(){
+		return 3;
+	}
+	public static void push(){
+
+	}
+}
