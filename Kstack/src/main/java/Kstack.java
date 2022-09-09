87ea3dd82ef7164b8cfd5bfa0359a2623cdad4ae
MyBeets <ikeller2023@kehillahstudent.org>
2022-09-09 09:13:11 -0700

    still cant get test 10 working

--- Kstack/src/main/java/Kstack.java
+++ Kstack/src/main/java/Kstack.java
@@ -8,20 +8,44 @@
  *
  * @author isabella
  */
+
 public class Kstack {
-    int[] array;
-    int idx;
+    private int[] array;
+    private int idx;
     
+    //constructors
     public Kstack(){
         array = new int[1];
         idx = 0;
     }
-    public void push(int a){
+    public Kstack(int n){
+        // add an exception thrower
+        array = new int[n];
+        idx = 0;
+    }
+    
+    //getters
+    public boolean empty(){
+        return getidx()==0;
+    }
+    public int[] getarr(){
+        return array;
+    }
+    public int getidx(){
+        return idx;
+    }
+    
+    //must implement
+    public void push(int a) throws RuntimeException{
+        // add an exception thrower
+        if(a>Integer.MAX_VALUE || a<Integer.MIN_VALUE) throw new RuntimeException("IllegalArgumentException");
         array[idx] = a;
         idx++;
     }
-    public int pop(){
-        int a = 0;
-        return a;
+    public int pop() throws RuntimeException{
+        // add an exception thrower
+        idx--;
+        if(idx<0) throw new RuntimeException("stack underflow");
+        return array[idx];
     }
 }
--- Kstack/src/main/java/Main.java
+++ Kstack/src/main/java/Main.java
@@ -8,20 +8,99 @@
  *
  * @author isabella
  */
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 public class Main {
+    private static Logger LOGGER = Logger.getLogger(Main.class.getName());
+    static int pass = 0;
+    static int fail = 0;
+    
     public static void main(String[] args){
+        // start
         System.out.println("running");
-        int pass = 0;
-        int fail = 0;
+        //LOGGER.log(Level.WARNING, "Failed stack construction expected empty got not empty");
+        
         Kstack k1 = new Kstack();
+        
         //test 1
-        if(k1.idx==0)pass++;
-        else fail++;
+        if(k1.empty())pass++;
+        else{
+            fail++;
+            logConError();
+        }
+        //test 1
+        if(k1.getidx()==0)pass++;
+        else{
+            fail++;
+            logConError();
+        }
         //test 2
-        if(k1.array[0]==0) pass++;
+        if(k1.getarr()[0]==0) pass++;
+        else{
+            fail++;
+            logConError();
+        }
+        
+        Kstack k2 = new Kstack(7);
+        //test 3
+        if(k2.getidx() == 0)pass++;
+        else{
+            fail++;
+            logConError();
+        }
+        // test 4
+        int check = 0;
+        for(int i = 0; i<7; i++){
+            if(k2.getarr()[i] == 0) check++;
+        }
+        if(check == 7) pass++;
+        else{
+            fail++;
+            logConError();
+        }
+        
+        //push tests
+        //test 5
+        try{ 
+            k2.push("mingus");
+            fail++;
+            LOGGER.log(Level.WARNING, "Failed push expected int got not int");
+        }
+        catch(Exception e){pass++;}
+        
+        //test 6
+        try{
+            k2.push(true);
+            fail++;
+            LOGGER.log(Level.WARNING, "Failed push expected int got not int");
+        }     
+        catch(Exception e){pass++;}
+        
+        //test 7
+        k2.push(1);
+        if(k2.getarr()[k2.getidx()] == 0)pass++;
         else fail++;
         
+        //test 8
+        if(k2.getarr()[k2.getidx()-1]==1)pass++;
+        else fail++;
+        
+        //test 9
+        //THIS IS BROKEN
+        try{
+            k2.push(Integer.MAX_VALUE+1);
+            fail++;
+        }     
+        catch(Exception e){pass++;}
+        
+        System.out.println(k2.getarr()[k2.getidx()-1]);
+        
+        
         //end
-        System.out.println(pass + "/" + pass+fail + "Tests Passed");
+        System.out.println(pass + "/" + (pass+fail) + " Tests Passed");
+    }
+    public static void logConError(){
+        LOGGER.log(Level.WARNING, "Failed stack construction expected empty got not empty");
     }
 }
