d5d3e887018c3df0d3ff97244f1c3ef6b2d44242
Mark <54454807+MyBeets@users.noreply.github.com>
2022-11-07 20:56:30 -0800

    Update Node.java

--- GraphAdjacencyMatrix/Node.java
+++ GraphAdjacencyMatrix/Node.java
@@ -1,1 +1,12 @@
-
+public class Node{
+    private int val;
+    public Node(int a){
+        val = a;
+    }
+    public int getNode(){
+        return val;
+    }
+    public String toString(){
+        return "" + val;
+    }
+}
